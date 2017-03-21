24b0110d2c594e323f077c3d3ed8f74c36e958d2
Ruben Barrado Jimenez <rbarrado@gamma11.aulas.gsyc.urjc.es>
2017-03-21 09:09:23 +0100

    Carpeta project

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from myfirstapp import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^suma/(.*)$', views.sumador, name="suma"),
+    url(r'^resta/(.*)$', views.restador, name="resta"),
+    url(r'^multi/(.*)$', views.multiplicador, name="multiplicador"),
+    url(r'^division/(.*)$', views.divisor, name="divisor"),
 )
